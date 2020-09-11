061df8947fdcb8984cfd8af42eca89046fdada70
Benjamin Stein <whereyouben@gmail.com>
2020-09-11 20:53:31 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,35 @@
+#display board method
+def display_board(board_state)
+  #row 1
+  puts " #{board_state[0]} | #{board_state[1]} | #{board_state[2]} "
+  puts "-----------"
+  #row 2
+  puts " #{board_state[3]} | #{board_state[4]} | #{board_state[5]} "
+  puts "-----------"
+  #row 3
+  puts " #{board_state[6]} | #{board_state[7]} | #{board_state[8]} "
+end
+
+#valid move method
+def valid_move?(board, index)
+  #if the move is within the board
+  if index.between?(0,8) == true
+    #and if the spot is not taken 
+    if position_taken?(board, index) == false
+      #valid move
+      true
+    end
+  else
+    #else not valid move
+    false
+  end
+end
+
+#position taken method to be used within valid move
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
